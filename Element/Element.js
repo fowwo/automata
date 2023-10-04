62ee3e1f85c3e84f60d9598f8fbe9111a58a8800
fowwo <54595444+fowwo@users.noreply.github.com>
2023-10-04 14:55:55 -0500

    Add element move listener capability

--- Element/Element.js
+++ Element/Element.js
@@ -6,6 +6,8 @@ export default class Element {
 	#x;
 	#y;
 
+	#moveListeners = new Set();
+
 	constructor(element, x, y) {
 		this.#element = element;
 		this.position = [ x, y ];
@@ -15,18 +17,33 @@ export default class Element {
 	get x() { return this.#x; }
 	set x(x) {
 		this.#x = x;
-		this.#element.style.setProperty("--x", `${x}px`);
+		this.element.style.setProperty("--x", `${x}px`);
+		this.#notifyMoveListeners();
 	}
 
 	get y() { return this.#y; }
 	set y(y) {
 		this.#y = y;
-		this.#element.style.setProperty("--y", `${y}px`);
+		this.element.style.setProperty("--y", `${y}px`);
+		this.#notifyMoveListeners();
 	}
 
 	get position() { return [ this.x, this.y ]; }
 	set position([ x, y ]) {
-		[ this.x, this.y ] = [ x, y ];
+		[ this.#x, this.#y ] = [ x, y ];
+		this.element.style.setProperty("--x", `${x}px`);
+		this.element.style.setProperty("--y", `${y}px`);
+		this.#notifyMoveListeners();
+	}
+
+	addMoveListener(listener) {
+		this.#moveListeners.add(listener);
+	}
+	removeMoveListener(listener) {
+		this.#moveListeners.delete(listener);
+	}
+	#notifyMoveListeners() {
+		for (const listener of this.#moveListeners) listener(this.position);
 	}
 
 }
