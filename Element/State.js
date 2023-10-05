fb920a1be3d889793b7b583eb8805f35fd53327e
fowwo <54595444+fowwo@users.noreply.github.com>
2023-10-04 23:53:25 -0500

    Add state labels

--- Element/State.js
+++ Element/State.js
@@ -2,14 +2,25 @@ import Draggable from "../Element/Draggable.js";
 
 export default class State extends Draggable {
 
+	#span;
 	#final;
 
-	constructor(x, y, { final = false } = {}) {
+	constructor(x, y, { label = "", final = false } = {}) {
 		const element = document.createElement("div");
 		element.classList.add("state");
 		super(element, x, y);
 
+		this.#span = document.createElement("span");
+		element.appendChild(this.#span);
+
 		this.final = final;
+		this.label = label;
+	}
+
+	get label() { return this.span.innerText; }
+	set label(value) {
+		this.#span.innerText = value;
+		this.#rescaleLabel();
 	}
 
 	get final() { return this.#final; }
@@ -20,6 +31,14 @@ export default class State extends Draggable {
 		} else {
 			this.element.classList.remove("final");
 		}
+		this.#rescaleLabel();
+	}
+
+	#rescaleLabel() {
+		const maxWidth = this.final ? 60 : 80;
+		const scale = this.#span.style.scale || 1;
+		const { width } = this.#span.getBoundingClientRect();
+		this.#span.style.scale = Math.min(1, scale * maxWidth / width);
 	}
 
 }
