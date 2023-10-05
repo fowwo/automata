521df89885aecec16d8338308f74ee846add5f09
fowwo <54595444+fowwo@users.noreply.github.com>
2023-10-05 16:28:52 -0500

    Add previous position to move listener

--- Element/Element.js
+++ Element/Element.js
@@ -20,24 +20,27 @@ export default class Element {
 
 	get x() { return this.#x; }
 	set x(x) {
+		const px = this.x;
 		this.#x = x;
 		this.element.style.setProperty("--x", `${x}px`);
-		this.#notifyMoveListeners();
+		this.#notifyMoveListeners([ px, this.y ]);
 	}
 
 	get y() { return this.#y; }
 	set y(y) {
+		const py = this.y;
 		this.#y = y;
 		this.element.style.setProperty("--y", `${y}px`);
-		this.#notifyMoveListeners();
+		this.#notifyMoveListeners([ this.x, py ]);
 	}
 
 	get position() { return [ this.x, this.y ]; }
 	set position([ x, y ]) {
+		const previous = this.position;
 		[ this.#x, this.#y ] = [ x, y ];
 		this.element.style.setProperty("--x", `${x}px`);
 		this.element.style.setProperty("--y", `${y}px`);
-		this.#notifyMoveListeners();
+		this.#notifyMoveListeners(previous);
 	}
 
 	addMoveListener(listener) {
@@ -46,8 +49,8 @@ export default class Element {
 	removeMoveListener(listener) {
 		this.#moveListeners.delete(listener);
 	}
-	#notifyMoveListeners() {
-		for (const listener of this.#moveListeners) listener(this.position);
+	#notifyMoveListeners(previous) {
+		for (const listener of this.#moveListeners) listener(this.position, previous);
 	}
 
 }
