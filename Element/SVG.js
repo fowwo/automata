b1e9c5a3a9f98e55c74dadcec8d18a101fc4d083
fowwo <54595444+fowwo@users.noreply.github.com>
2023-10-08 02:47:04 -0500

    Add SVG element

---
+++ Element/SVG.js
@@ -0,0 +1,18 @@
+import Element from "./Element.js";
+
+/** An SVG element in the workspace. */
+export default class SVG extends Element {
+
+	/**
+	 * @param {Number} x - The initial x position.
+	 * @param {Number} y - The initial y position.
+	 */
+	constructor(x, y) {
+		const element = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+		element.setAttribute("width", "1");
+		element.setAttribute("height", "1");
+		element.setAttribute("viewBox", "-0.5 -0.5 1 1");
+		super(element, x, y);
+	}
+
+}
