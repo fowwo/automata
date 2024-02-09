c142f0493f0a1ef480431d02dad12aabd205137b
key <54595444+fowwo@users.noreply.github.com>
2024-02-09 00:05:13 -0600

    Add support for loop transitions

--- Element/Transition.js
+++ Element/Transition.js
@@ -4,6 +4,9 @@ import State, { radius as stateRadius } from "./State.js";
 import StraightArrow from "./StraightArrow.js";
 import SVG from "./SVG.js";
 
+const loopDistance = 50;
+const loopAngle = -Math.PI / 2;
+
 /** A transition in a diagram. */
 export default class Transition extends SVG {
 
@@ -20,6 +23,7 @@ export default class Transition extends SVG {
 
 	// Listeners
 	#onStateMove;
+	#onFromMove;
 	#onToMove;
 	#onAnchorMove;
 
@@ -29,7 +33,7 @@ export default class Transition extends SVG {
 	 * @param {String} label - The label of the transition.
 	 * @param {Number} [angle] - The angle of the transition.
 	 */
-	constructor(from, to, label, angle = 0) {
+	constructor(from, to, label, angle = from === to ? loopAngle : 0) {
 		super(...midpoint(from.position, to.position));
 
 		const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
@@ -53,6 +57,20 @@ export default class Transition extends SVG {
 				const [ bx, by ] = this.#to.position;
 				const [ cx, cy ] = midpoint(this.#from.position, this.#to.position);
 				const [ dx, dy ] = [ bx - ax, by - ay ];
+
+				if (this.#from === this.#to) {
+					const distance = stateRadius + loopDistance;
+					const initialAngle = Math.atan2(y - cy, x - cx);
+					[ x, y ] = [ distance * Math.cos(initialAngle) + cx, distance * Math.sin(initialAngle) + cy ];
+
+					const threshold = 10;
+					if (Math.abs(x - cx) < threshold) x = cx;
+					else if (Math.abs(y - cy) < threshold) y = cy;
+					else return [ x, y ];
+
+					const newAngle = Math.atan2(y - cy, x - cx);
+					return [ distance * Math.cos(newAngle) + cx, distance * Math.sin(newAngle) + cy ];
+				}
 
 				const getPosition = (x, y) => {
 					// Handle horizontal and vertical lines.
@@ -83,6 +101,10 @@ export default class Transition extends SVG {
 
 			this.#renderPath();
 		}
+		this.#onFromMove = () => {
+			if (this.#from === this.#to) return;
+			this.#onStateMove();
+		}
 		this.#onToMove = () => {
 			this.#arrowHead.position = this.#to.position;
 			this.#onStateMove();
@@ -98,36 +120,47 @@ export default class Transition extends SVG {
 			const [ cx, cy ] = midpoint(this.#from.position, this.#to.position);
 			const [ dx, dy ] = [ bx - ax, by - ay ];
 
-			const m = Math.sqrt((cx - x) ** 2 + (cy - y) ** 2);
-			const r = Math.sqrt(dx ** 2 + dy ** 2) / 2;
-			const t = Math.atan2(dy, dx);
-			const a = Math.atan2(y - cy, x - cx);
+			if (this.#from === this.#to) {
+				this.#angle = Math.atan2(y - cy, x - cx);
+			} else {
+				const m = Math.sqrt((cx - x) ** 2 + (cy - y) ** 2);
+				const r = Math.sqrt(dx ** 2 + dy ** 2) / 2;
+				const t = Math.atan2(dy, dx);
+				const a = Math.atan2(y - cy, x - cx);
+	
+				const angle = 2 * Math.atan2(m, r + stateRadius);
+				const flip = (t - a + 2 * Math.PI) % (2 * Math.PI) < Math.PI;
+				this.#angle = flip ? -angle : angle;
+			}
 
-			const angle = 2 * Math.atan2(m, r + stateRadius);
-			const flip = (t - a + 2 * Math.PI) % (2 * Math.PI) < Math.PI;
-			this.#angle = flip ? -angle : angle;
 			this.#renderPath();
 			this.#repositionLabel();
 		}
 
-		this.#from.addMoveListener(this.#onStateMove);
+		this.#from.addMoveListener(this.#onFromMove);
 		this.#to.addMoveListener(this.#onToMove);
 		this.#anchor.addMoveListener(this.#onAnchorMove);
 	}
 
 	get from() { return this.#from; }
 	set from(state) {
-		this.#from.removeMoveListener(this.#onStateMove);
+		if (this.#from === state) return;
+
+		this.#from.removeMoveListener(this.#onFromMove);
 		this.#from = state;
-		this.#from.addMoveListener(this.#onStateMove);
+		this.#from.addMoveListener(this.#onFromMove);
+		this.#angle = this.#from === this.#to ? loopAngle : 0;
 		this.#onStateMove();
 	}
 
 	get to() { return this.#to; }
 	set to(state) {
+		if (this.#to === state) return;
+
 		this.#to.removeMoveListener(this.#onToMove);
 		this.#to = state;
 		this.#to.addMoveListener(this.#onToMove);
+		this.#angle = this.#from === this.#to ? loopAngle : 0;
 		this.#onToMove();
 	}
 
@@ -138,6 +171,18 @@ export default class Transition extends SVG {
 	}
 
 	#renderPath() {
+		if (this.#from === this.#to) {
+			const angle = 2 * Math.atan2(stateRadius + loopDistance, stateRadius);
+			const r = -stateRadius * Math.cos(angle) / Math.sin(angle);
+			
+			const [ x, y ] = [ stateRadius * Math.cos(angle), stateRadius * Math.sin(angle) ];
+			
+			this.#path.setAttribute("d", `M ${x} ${y} A ${r} ${r} 0 1 0 ${-x} ${y}`);
+			this.#arrowHead.element.style.rotate = `${this.#angle + angle - Math.PI / 2}rad`;
+			this.element.style.rotate = `${this.#angle - Math.PI / 2}rad`;
+			return;
+		}
+
 		const [ ax, ay ] = this.#from.position;
 		const [ bx, by ] = this.#to.position;
 		const [ rx, ry ] = [ (bx - ax) / 2, (by - ay) / 2 ];
@@ -152,6 +197,7 @@ export default class Transition extends SVG {
 		const flip = this.#angle < 0;
 		this.#path.setAttribute("d", `M ${x1} ${y1} A ${r} ${r} 0 ${large ? 1 : 0} ${flip ? 1 : 0} ${x2} ${y2}`);
 		this.#arrowHead.element.style.rotate = `${Math.PI - this.#angle + t}rad`;
+		this.element.style.rotate = "";
 	}
 	#repositionLabel() {
 		const [ ax, ay ] = this.#from.position;
@@ -165,6 +211,14 @@ export default class Transition extends SVG {
 		const a = Math.atan2(y - cy, x - cx);
 		const angle = t + Math.PI / 2;
 		const distance = 10;
+
+		if (this.#from === this.#to) {
+			this.#label.position = [
+				x + (distance + width / 2) * Math.cos(this.#angle),
+				y + (distance + height / 2) * Math.sin(this.#angle)
+			];
+			return;
+		}
 
 		if (this.#angle === 0) {
 			const flip = 0 <= angle && angle <= Math.PI;
@@ -194,6 +248,10 @@ export default class Transition extends SVG {
 	}
 	#getArcMidpoint() {
 		const [ cx, cy ] = midpoint(this.#from.position, this.#to.position);
+		if (this.#from === this.#to) {
+			const distance = stateRadius + loopDistance;
+			return [ cx + distance * Math.cos(this.#angle), cy + distance * Math.sin(this.#angle) ];
+		}
 		if (this.#angle === 0) return [ cx, cy ];
 
 		const [ ax, ay ] = this.#from.position;
