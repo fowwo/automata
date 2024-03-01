74a08a712c810b39229cf2e32178ae34d3b827de
key <54595444+fowwo@users.noreply.github.com>
2024-03-01 00:54:37 -0600

    Show states in diagram info

--- Model/Diagram.js
+++ Model/Diagram.js
@@ -24,13 +24,64 @@ export default class Diagram {
 	/** Renders the diagram. */
 	load() {
 		document.getElementById("diagram").innerHTML = "";
+		const statesList = document.querySelector("#states tbody");
+		statesList.innerHTML = "";
 
-		// Render states.
+		// Render states and display state info.
+		let startState = null;
 		const states = this.elements.states.map(({ x, y, ...options }, i) => {
 			if (i === this.machine.startState) options.start = this.elements.startState;
 			if (this.machine.finalStates.has(i)) options.final = true;
 			
-			return new State(x, y, options);
+			const state = new State(x, y, options);
+			const tr = document.createElement("tr");
+
+			// Start state toggle
+			const radio = document.createElement("div");
+			radio.classList.add("radio", "symbol");
+			const start = document.createElement("input");
+			start.type = "radio";
+			start.name = "start";
+			if (state.start) {
+				startState = state;
+				start.checked = true;
+			}
+			start.addEventListener("change", () => {
+				if (startState) {
+					state.start = startState.start;
+					startState.start = null;
+				}
+				startState = state;
+			});
+			radio.appendChild(start);
+
+			// Final state toggle
+			const checkbox = document.createElement("div");
+			checkbox.classList.add("checkbox", "symbol");
+			const final = document.createElement("input");
+			final.type = "checkbox";
+			final.checked = state.final;
+			final.addEventListener("change", (event) => {
+				state.final = event.target.checked;
+			});
+			checkbox.appendChild(final);
+
+			// Label
+			const label = document.createElement("input");
+			label.type = "text";
+			label.value = state.label;
+			label.addEventListener("change", (event) => {
+				state.label = event.target.value;
+			});
+			
+			// Append elements to table row.
+			for (const element of [ radio, checkbox, label ]) {
+				const td = document.createElement("td");
+				td.appendChild(element);
+				tr.appendChild(td);
+			}
+			statesList.appendChild(tr);
+			return state;
 		});
 
 		// Render transitions.
