38b438d5724430375fcde045e5473cfbaee4b42a
key <54595444+fowwo@users.noreply.github.com>
2024-05-08 18:52:30 -0500

    Convert Element to component

---
+++ View/Component/Diagram/Element/Element.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+	defineProps<{
+		x: number;
+		y: number;
+	}>();
+</script>
+<template>
+	<div class="element">
+		<slot></slot>
+	</div>
+</template>
+<style scoped>
+	.element {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		translate:
+			calc(var(--scale, 1) * calc(var(--px, 0) + v-bind("x")) * 1px - 50%)
+			calc(var(--scale, 1) * calc(var(--py, 0) + v-bind("y")) * 1px - 50%);
+		scale: var(--scale, 1);
+	}
+</style>
