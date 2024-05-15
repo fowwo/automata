ad96a723cb3fbaf59d50071e7404ae561294507b
key <54595444+fowwo@users.noreply.github.com>
2024-05-15 12:15:09 -0500

    Adjust Text size and alignment

--- View/Component/Diagram/Element/Text.vue
+++ View/Component/Diagram/Element/Text.vue
@@ -19,7 +19,9 @@
 </template>
 <style scoped>
 	.text {
-		font-size: 32px;
+		font-size: 1.75em;
+		line-height: 1;
+		text-align: center;
 		text-shadow:
 			-1px -1px 0px hsl(var(--background-color-hsl)),
 			-1px 1px 0px hsl(var(--background-color-hsl)),
