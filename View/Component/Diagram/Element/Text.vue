8c01f4392d12da9f3fa0564b858233715b1767eb
key <54595444+fowwo@users.noreply.github.com>
2024-05-14 21:47:01 -0500

    Add Text component

---
+++ View/Component/Diagram/Element/Text.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+	import { ref } from "vue";
+	import Element from "./Element.vue";
+	
+	defineProps<{
+		x: number;
+		y: number;
+		value: string;
+	}>();
+
+	const span = ref<HTMLSpanElement | null>(null);
+
+	defineExpose({ span });
+</script>
+<template>
+	<Element :x :y class="text">
+		<span ref="span">{{ value }}</span>
+	</Element>
+</template>
+<style scoped>
+	.text {
+		font-size: 32px;
+		text-shadow:
+			-1px -1px 0px hsl(var(--background-color-hsl)),
+			-1px 1px 0px hsl(var(--background-color-hsl)),
+			1px -1px 0px hsl(var(--background-color-hsl)),
+			1px 1px 0px hsl(var(--background-color-hsl));
+		z-index: 2;
+	}
+</style>
