147ca15e74b450bfd79b0b89774c34d4b6cc7197
key <54595444+fowwo@users.noreply.github.com>
2024-05-20 22:54:57 -0500

    Add Input auto width class

--- View/Component/Input/Text.vue
+++ View/Component/Input/Text.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
 	import Input from "./Input.vue";
+
+	defineProps<{ autoWidth?: boolean }>();
+
+	const model = defineModel();
 </script>
 <template>
-	<Input type="text" class="text" @keydown.enter="$event.target.blur()" size="1" />
+	<Input type="text" class="text" :class="{ autoWidth }" @keydown.enter="$event.target.blur()" size="1" v-model="model" :data-value="model" />
 </template>
 <style scoped>
 	.text {
@@ -26,6 +30,22 @@
 			:deep(&):focus, :deep(&):enabled:hover {
 				color: hsl(var(--text-color-hsl) / 100%);
 			}
+		}
+	}
+	.autoWidth {
+		display: inline-grid;
+		width: auto;
+
+		& > :deep(input) {
+			text-align: center;
+			grid-area: 1 / 1;
+		}
+		&::after {
+			content: attr(data-value);
+			visibility: hidden;
+			margin: 0 10px;
+			white-space: pre;
+			grid-area: 1 / 1;
 		}
 	}
 </style>
