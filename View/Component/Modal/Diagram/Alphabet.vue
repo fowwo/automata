e323b064a4ad24065c28e91ce528db1a1d38b346
key <54595444+fowwo@users.noreply.github.com>
2024-05-24 14:03:06 -0500

    Add tape symbols to Alphabet page

--- View/Component/Modal/Diagram/Alphabet.vue
+++ View/Component/Modal/Diagram/Alphabet.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
 	import { nextTick, ref } from "vue";
 	import Diagram from "../../../../Model/Diagram";
+	import TextInput from "../../Input/Text.vue";
 	import TextValidator from "../../Input/TextValidator.vue";
+	import TuringMachineDiagram from "../../../../Model/Diagram/TuringMachine";
 
 	const props = defineProps<{ diagram: Diagram; }>();
 
 	const symbols = ref<HTMLUListElement | null>(null);
+	const tapeSymbols = ref<HTMLUListElement | null>(null);
 
 	function isValidSymbol(index: number, symbol: string) {
 		if (symbol === "") return false;
@@ -21,13 +24,18 @@
 		await nextTick();
 		(symbols.value?.children[symbols.value.children.length - 2].querySelector("input[type=text]") as HTMLInputElement).focus();
 	}
+	async function addTapeSymbol() {
+		(props.diagram as TuringMachineDiagram).automaton.addTapeSymbol();
+		await nextTick();
+		(tapeSymbols.value?.children[tapeSymbols.value.children.length - 2].querySelector("input[type=text]") as HTMLInputElement).focus();
+	}
 </script>
 <template>
 	<div class="alphabet">
 		<h1>Alphabet</h1>
 		<hr>
 		<h2>Input</h2>
-		<p>The set of symbols allowed in an input string.</p>
+		<p>The set of symbols allowed in the input string.</p>
 		<ul ref="symbols">
 			<li v-for="symbol, i in diagram.automaton.alphabet">
 				<TextValidator revert auto-width
@@ -44,6 +52,30 @@
 				<button class="symbol small-shadow" @click="addSymbol()">&#xE145;</button>
 			</li>
 		</ul>
+		<template v-if="(diagram instanceof TuringMachineDiagram)">
+			<hr>
+			<h2>Tape</h2>
+			<p>The set of symbols allowed on the tape.</p>
+			<ul ref="tapeSymbols">
+				<li v-for="symbol in diagram.automaton.alphabet">
+					<TextInput auto-width disabled :modelValue="symbol" />
+				</li>
+				<li v-for="symbol, i in diagram.automaton.tapeAlphabet">
+					<TextValidator revert auto-width
+						:validator="(symbol) => isValidSymbol(i + diagram.automaton.alphabet.length, symbol)"
+						v-model.trim.collapse.lazy="diagram.automaton.tapeAlphabet[i]"
+						@change="() => {
+							const newSymbol = (diagram as TuringMachineDiagram).automaton.tapeAlphabet[i];
+							(diagram as TuringMachineDiagram).automaton.tapeAlphabet[i] = symbol; // Revert v-model change
+							diagram.automaton.renameSymbol(symbol, newSymbol);
+						}"
+					/>
+				</li>
+				<li>
+					<button class="symbol small-shadow" @click="addTapeSymbol()">&#xE145;</button>
+				</li>
+			</ul>
+		</template>
 	</div>
 </template>
 <style scoped>
